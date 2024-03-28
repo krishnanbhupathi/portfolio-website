1916020f8d58871f147b5fef638e5a7869e290cd
krishnanbhupathi <krishnanbhupathi@gmail.com>
2024-03-28 12:32:59 +0530

    Initial commit: Added initial page structure  and stylings

---
+++ krishna/src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a
+        href='https://www.linkedin.com/in/murali-krishna-b-913236190/'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-linkedin'></i>
+      </a>
+      <a
+        href='https://github.com/krishnanbhupathi'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-github'></i>
+      </a>
+      <a
+        href='https://www.instagram.com/krishnanbhupathii/'
+        className='home__social-icon'
+        target='_blank'
+      >
+        <i class='uil uil-instagram'></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
