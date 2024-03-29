7b336a90db8a1f662a19a48831686c4b64b28ee0
krishnanbhupathi <krishnanbhupathi@gmail.com>
2024-03-29 20:49:28 +0530

    Added skills component to website

---
+++ krishna/src/components/skills/Backend.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className='skills__content'>
+      <h3 className='skills__title'>Backend</h3>
+
+      <div className='skills__box'>
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Java</h3>
+              <span className='skill__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Spring</h3>
+              <span className='skill__level'>Basic</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>Python</h3>
+              <span className='skill__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>c , c++</h3>
+              <span className='skill__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>MySQL</h3>
+              <span className='skill__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className='skills__name'>AWS</h3>
+              <span className='skill__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Backend
